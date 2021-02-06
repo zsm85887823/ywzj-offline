0c542af9966c4d8440e61b68d4c262ba7c396042
Andiliba <648474134@qq.com>
2021-02-06 13:18:59 +0800

    增加其他服务器，跨域问题未解决

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home";
 import Offline from "@/views/Offline";
-
+import Test from "@/views/Test";
 
 const history = createWebHistory();
 
 const routes = [
     {
-        path: "/",
+        path: "/home",
         component: Home,
     },
     {
         path: "/offline",
         component: Offline,
+    },
+    {
+        path: "/test",
+        component: Test,
     }
 ]
 
