93cdaa020deec99166e704a207b9b0be0adec7be
Andiliba <648474134@qq.com>
2021-02-12 13:16:45 +0800

    更新

--- src/layout/components/AccountSetting.vue
+++ src/layout/components/AccountSetting.vue
@@ -1,47 +1,44 @@
 <template>
-    <a-modal
-        :visible="props.visiable"
-        title="账户管理"
-        :footer="null"
-        width="90vh"
-    >
-        <a-row>
-            <a-col :span="11">
-                <h3 style="text-align: center">账户列表</h3>
-                <span v-show="accountList && accountList.length == 0">尚无账户</span>
-
-            </a-col>
-            <a-col :span="2">
-                <a-divider type="vertical" style="height: 100%" />
-            </a-col>
-            <a-col :span="11">
-                <h3 style="text-align: center">新增账户</h3>
-                <a-form
-                    width="100%"
-                    :model="account"
-                    labelAlign="right"
-                    :labelCol="{ span: 4 }"
-                    :wrapperCol="{ span: 20 }"
-                >
-                    <a-form-item label="账户名" name="username">
-                        <a-input v-model:value="account.username" />
-                    </a-form-item>
-                    <a-form-item label="密码" name="password">
-                        <a-input
-                            v-model:value="account.password"
-                            type="password"
-                        />
-                    </a-form-item>
-                </a-form>
-                <a-button
-                    type="primary"
-                    style="width: 8em; float: right"
-                    @click="addAccount"
-                    >确认</a-button
-                >
-            </a-col>
-        </a-row>
-    </a-modal>
+  <a-modal
+    :visible="props.visiable"
+    title="账户管理"
+    :footer="null"
+    width="90vh"
+    breakpoint="lg"
+  >
+    <a-row>
+      <a-col :span="11">
+        <h3 style="text-align: center">账户列表</h3>
+        <span v-show="accountList && accountList.length == 0">尚无账户</span>
+      </a-col>
+      <a-col :span="2">
+        <a-divider type="vertical" style="height: 100%" />
+      </a-col>
+      <a-col :span="11">
+        <h3 style="text-align: center">新增账户</h3>
+        <a-form
+          width="100%"
+          :model="account"
+          labelAlign="right"
+          :labelCol="{ span: 4 }"
+          :wrapperCol="{ span: 20 }"
+        >
+          <a-form-item label="账户名" name="username">
+            <a-input v-model:value="account.username" />
+          </a-form-item>
+          <a-form-item label="密码" name="password">
+            <a-input v-model:value="account.password" type="password" />
+          </a-form-item>
+        </a-form>
+        <a-button
+          type="primary"
+          style="width: 8em; float: right"
+          @click="addAccount"
+          >确认</a-button
+        >
+      </a-col>
+    </a-row>
+  </a-modal>
 </template>
 
 <script>
@@ -50,39 +47,47 @@ import { useStore } from "vuex";
 import { originApis } from "@/api";
 
 export default defineComponent({
-    name: "AccountSetting",
-    props: {
-        visiable: Boolean,
-    },
-    setup(props) {
-        const store = useStore()
-        const accountList  = ref(store.getters.accountList || []);
+  name: "AccountSetting",
+  props: {
+    visiable: Boolean,
+  },
+  setup(props) {
+    const store = useStore();
+    const accountList = ref(store.getters.accountList || []);
 
-        const account = reactive({
-            username: "miyling",
-            password: "miyling",
+    const account = reactive({
+      username: "miyling",
+      password: "miyling",
+    });
+
+    const addAccount = async () => {
+      try {
+        const res = await originApis.login(account);
+
+        store.dispatch("account/addAcccount", {
+          ...account,
+          nickName: res.data.nickname,
         });
 
-        const addAccount = async () => {
-            try {
-                const res = await originApis.login(account);
-            
-                store.dispatch("account/addAcccount", { ...account, nickName: res.data.nickname})
+        console.log(res);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-                console.log(res)
-            } catch (err) {
-                console.log(err)
-            }
-        };
+    watch(
+      () => store.state.account.accountList,
+      (newV) => ((accountList.value = newV)),
 
-        watch(() => store.state.account.accountList, (newV) => accountList.value = newV, { deep: true})
+      { deep: true }
+    );
 
-        return {
-            props,
-            accountList,
-            account,
-            addAccount
-        };
-    },
+    return {
+      props,
+      accountList,
+      account,
+      addAccount,
+    };
+  },
 });
 </script>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,39 +1,38 @@
 <template>
+  <a-layout>
+    <a-layout-sider breakpoint="lg" collapsed-width="0" width="100">
+      <div style="height: 64px" />
+      <a-menu
+        theme="dark"
+        mode="inline"
+        v-model:selectedKeys="chooseRoute"
+        :style="{ borderRight: 0 }"
+      >
+        <a-menu-item v-for="route in permission_routes" :key="route.path">
+          <router-link :to="route.path">
+            {{ route.name }}
+          </router-link>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
     <a-layout>
-        <a-layout-sider width="200">
-            <div style="height: 64px" />
-            <a-menu
-                theme="dark"
-                mode="inline"
-                v-model:selectedKeys="chooseRoute"
-                :style="{ borderRight: 0 }"
-            >
-                <a-menu-item v-for="route in permission_routes" :key="route.path">
-                    <router-link :to="route.path">
-                        {{ route.name }}
-                    </router-link>
-                </a-menu-item>
-            </a-menu>
-        </a-layout-sider>
-        <a-layout>
-            <a-layout-header>
-                <div style="float: right">
-                    <span style="color: #fff; margin-right: 10px"
-                        >当前账户:
-                        {{ nickName }}</span
-                    >
-                    <a-button type="primary" @click="showSetting"> 账户管理 </a-button>
-                </div>
-            </a-layout-header>
-            <a-layout-content style="min-height: 85vh">
-                <router-view />
-            </a-layout-content>
-            <a-layout-footer>
-                <span>Andy是🐷</span>
-            </a-layout-footer>
-        </a-layout>
+      <a-layout-header>
+        <div style="float: right">
+          <span style="color: #fff; margin-right: 10px"
+            >当前账户: {{ nickName }}</span
+          >
+          <a-button type="primary" @click="showSetting"> 账户管理 </a-button>
+        </div>
+      </a-layout-header>
+      <a-layout-content style="min-height: 85vh">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer>
+        <span>Andy是🐷</span>
+      </a-layout-footer>
     </a-layout>
-    <AccountSetting :visiable="visiable" @cancel="closeSetting" />
+  </a-layout>
+  <AccountSetting :visiable="visiable" @cancel="closeSetting" />
 </template>
 
 <script>
@@ -42,38 +41,39 @@ import AccountSetting from "./components/AccountSetting";
 import { useStore } from "vuex";
 
 export default defineComponent({
-    name: "Layout",
-    components: {
-        AccountSetting
-    },
-    setup() {
-     
-        const store = useStore();
-        const { currentAccount, permission_routes } = store.getters;
-        const nickName = ref(currentAccount ? currentAccount.nickName : "尚无账户")
-        const chooseRoute = ref([permission_routes[0].path]);
+  name: "Layout",
+  components: {
+    AccountSetting,
+  },
+  setup() {
+    const store = useStore();
+    const { currentAccount, permission_routes } = store.getters;
+    const nickName = ref(currentAccount ? currentAccount.nickName : "尚无账户");
+    const chooseRoute = ref([permission_routes[0].path]);
 
-        const visiable = ref(false);
+    const visiable = ref(false);
 
-        const showSetting = () => {
-            visiable.value = true;
-        }
+    const showSetting = () => {
+      visiable.value = true;
+    };
 
-        const closeSetting = () => {
-            visiable.value = false;
-        }
+    const closeSetting = () => {
+      visiable.value = false;
+    };
 
+    watch(
+      () => store.state.account.currentAccount,
+      (newAccount) => (nickName.value = newAccount.nickname)
+    );
 
-        watch(() => store.state.account.currentAccount, (newAccount) => nickName.value = newAccount.nickname)
-        
-        return {
-            nickName,
-            permission_routes,
-            chooseRoute,
-            visiable,
-            showSetting,
-            closeSetting
-        };
-    },
+    return {
+      nickName,
+      permission_routes,
+      chooseRoute,
+      visiable,
+      showSetting,
+      closeSetting,
+    };
+  },
 });
 </script>
